2ba9a7c36a9a74bec02fc8141cd81ba6d8c71020
DominusTrax <33012690+DominusTrax@users.noreply.github.com>
2021-02-12 17:48:10 -0500

    Update player_esp.lua

--- eclipsis/player_esp.lua
+++ eclipsis/player_esp.lua
@@ -5,3 +5,26 @@ if game.CoreGui:FindFirstChild("PlayerESP") then
   return print("[Player ESP]: You already executed the Player ESP!")
 end
 
+local Player = game.Players.LocalPlayer
+
+local Folder = Instance.new("Folder", game.CoreGui)
+Folder.Name = "PlayerESP"
+
+function ESP(object)
+  local Box = Instance.new("BoxHandleAdornment")
+  Box.Size = Vector3.new(1.6, 1.9, 0.8)
+  Box.Adornee = object.HumanoidRootPart
+  Box.AlwaysOnTop = true
+  Box.Transparency = 0.5
+  Box.ZIndex = 10
+  Box.Color3 = game.Players[object.Name].TeamColor.Color
+  Box.Parent = Folder
+end
+
+for i,v in pairs(game.Players:GetPlayers()) do
+  if v.TeamColor ~= Player.TeamColor then
+      ESP(v.Character)
+    else
+      print("[Player ESP]: Why would we use ESP on your player now?")
+    end
+end
