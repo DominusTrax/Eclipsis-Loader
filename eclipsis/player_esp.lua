c86a10d2df78e39cffd3f3bf459eeb196e88a174
DominusTrax <33012690+DominusTrax@users.noreply.github.com>
2021-02-12 17:37:22 -0500

    Update player_esp.lua

--- eclipsis/player_esp.lua
+++ eclipsis/player_esp.lua
@@ -1,2 +1,7 @@
--- Credits to Derpy Lua
+-- All credits to Derpy Lua
 -- 2/12/2021
+
+if game.CoreGui:FindFirstChild("PlayerESP") then
+  return print("[Player ESP]: You already executed the Player ESP!")
+end
+
