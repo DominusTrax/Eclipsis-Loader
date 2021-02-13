69d85d10380f6e0bc5170d6477fb3a594b87ccea
DominusTrax <33012690+DominusTrax@users.noreply.github.com>
2021-02-13 09:31:46 -0500

    Update eclipsis_loading.lua

--- eclipsis_loading.lua
+++ eclipsis_loading.lua
@@ -1,15 +1,60 @@
-local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/DominusTrax/Eclipsis-Loader/main/uilib/library.lua"))()
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/3gQQtaKX"))()
 
 -- Starts a new UI
-local EclipsisWindow = Library:NewWindow("Eclipsis")
+local EclipsisWindow = Library:CreateTab("Eclipsis")
 
 -- Adds sections into the UI
-local Main = EclipsisWindow:NewSection("Main")
+local Main = EclipsisWindow:CreateSection("Main")
 
-Main:CreateToggle("Test Button", function(enabled)
+-- Adds the "gun" section
+local GunSection = EclipsisWindow:CreateSection("Gun Section")
+
+-- Everything below is basically common knowledge.
+
+Main:CreateButton("Player ESP", function()
+    print("Pushed PlrESP")
+end)
+
+Main:CreateButton("Spawn ESP", function()
+    print("Pushed SpawnESP")
+end)
+
+Main:CreateToggle("Infinite Jetpack", function(enabled)
     if enabled then 
         print(enabled)
     elseif not enabled then
         print(enabled)
     end
+end)
+
+Main:CreateToggle("Iridium Collector", function(enabled)
+    if enabled then 
+        print(enabled)
+    elseif not enabled then
+        print(enabled)
+    end
+end)
+
+Main:CreateToggle("Staff Detection", function(enabled)
+    if enabled then 
+        print(enabled)
+    elseif not enabled then
+        print(enabled)
+    end
+end)
+
+GunSection:CreateSlider("Damage",1,500,1,1,function(value)
+    print("Damage:" .. value)
+end)
+
+GunSection:CreateSlider("Range",1,500,1,1,function(value)
+    print("Range:" .. value)
+end)
+
+GunSection:CreateSlider("Firerate",1,500,1,1,function(value)
+    print("Firerate:" .. value)
+end)
+
+GunSection:CreateSlider("Accuracy",1,10,1,1,function(value)
+    print("Accuracy:" .. value)
 end)
